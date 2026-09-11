08cbd1d56718088cb8d1c5d39f25d7133185c6a8
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-09-11 16:50:17 -0300

    empiezo curso de security

---
+++ 3-ApiConSecurity/src/main/java/com/apisimple/_apiconsecurity/controller/CalificationsController.java
@@ -0,0 +1,35 @@
+package com.apisimple._apiconsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@RestController
+public class CalificationsController {
+
+    public static int[] notas = new int[8];
+
+    @GetMapping("/califications")
+    public String devolverCalificaciones() {
+        Random r = new Random();
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = r.nextInt(1, 11);
+        }
+        
+        return Arrays.toString(notas);
+    }
+
+    @GetMapping("/califications/average")
+    public String promedioCalificaciones() {
+        float promedio = (float) Arrays.stream(notas).sum() / notas.length;
+
+        return "El promedio de las notas es: " + promedio;
+    }
+
+}
+
+
+
+
---
+++ 3-ApiConSecurity/src/main/java/com/apisimple/_apiconsecurity/controller/HolaMundo.java
@@ -0,0 +1,27 @@
+package com.apisimple._apiconsecurity.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PreAuthorize("denyAll()") // Se deniega t0do acceso sin autentificar
+public class HolaMundo {
+
+    @GetMapping("/holaSeg")
+    @PreAuthorize("hasAuthority('READ')") // Todos los que tienen permiso de lectura pueden entrar
+    public String secHolaMundo() {
+        return "Hola Mundo con Security";
+    }
+
+    @GetMapping("/holaNoSeg")
+    @PreAuthorize("permitAll()")
+    public String noSecHolaMundo() {
+        return "Hola Mundo sin Security";
+    }
+
+    @GetMapping("/holaNoSegSinAnnotation") //
+    public String noSecAnnotationHolaMundo() {
+        return "Hola Mundo con seguridad debido al denyall de arriba de todo";
+    }
+}
