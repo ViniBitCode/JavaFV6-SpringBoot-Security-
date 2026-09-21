b671c1d4236821720cf3fac739b5f0653ad7d9aa
Facu Vini <67282200+ViniBitCode@users.noreply.github.com>
2026-09-21 20:08:54 -0300

    Elimino carpeta para que solo tenga el ejercicio de la raman

--- 3-ApiConSecurity/src/main/java/com/apisimple/_apiconsecurity/dto/AuthResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package com.apisimple._apiconsecurity.dto;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-// Esta annotation me permite asegurarme armar Json de objetos en un orden específico
-@JsonPropertyOrder({"username", "message", "jwt", "status"})
-public record AuthResponseDTO(String username, String message, String jwt, boolean status) {
-}
