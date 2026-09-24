f610188f162ba60c87ec182d7d4c5e88a2e2698b
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-09-24 14:39:05 -0300

    Cambio env porque puse el realm duplicado :

--- 4.1-ApiConKeycloak/Frontend/src/environments/environment.ts
+++ 4.1-ApiConKeycloak/Frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment: AppEnvironment = {
   production: true,
   apiBaseUrl: 'https://javafv6-springboot-security-1.onrender.com',
   keycloak: {
-    url: 'https://lemur-10.cloud-iam.com/auth/realms/primer-api-keycloak-hosteada',
+    url: 'https://lemur-10.cloud-iam.com/auth/',
     realm: 'primer-api-keycloak-hosteada',
     clientId: 'angular-app',
   },
