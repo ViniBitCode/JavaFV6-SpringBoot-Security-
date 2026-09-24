eb91e6ce66fe08351259f23d6ae6b82ae310af86
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-09-24 14:34:15 -0300

    Agrego url del front en el CORS de la api

--- 4.1-ApiConKeycloak/Backend/src/main/java/com/apisimple/_apiconkeycloak/shared/security/SecurityConfig.java
+++ 4.1-ApiConKeycloak/Backend/src/main/java/com/apisimple/_apiconkeycloak/shared/security/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "https://stalwart-taffy-a1a0ac.netlify.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
